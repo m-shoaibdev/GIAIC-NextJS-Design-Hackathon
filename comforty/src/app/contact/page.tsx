2d4dd9ef750662222d738722799fc52491033a6b
m-shoaibdev <shoaibaas99@gmail.com>
2024-12-10 00:52:05 +0500

    responsivness improved

--- comforty/src/app/contact/page.tsx
+++ comforty/src/app/contact/page.tsx
@@ -26,11 +26,11 @@ export default function Contact() {
                         
                     </div>
                     <div className="mb-4">
-                        <button className="bg-primary text-white text-xs md:text-sm py-1 md:py-2 px-2 md:px-4 rounded-lg outline-none focus:outline-none">Submit</button>
+                        <button className="bg-primary text-white text-base md:text-lg py-2.5 md:py-3 px-4 md:px-6 rounded-lg outline-none focus:outline-none">Submit</button>
                     </div>
                 </div>
                 <div className="p-4 flex flex-col gap-12 mt-10">
-                <div className="flex gap-2 md:w-80">
+                <div className="flex gap-4 md:w-80">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 md:size-16">
   <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
 </svg>
@@ -40,7 +40,7 @@ export default function Contact() {
                     </div>
                 </div>
 
-                <div className="flex gap-2 md:w-72">
+                <div className="flex gap-4 md:w-80">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 md:size-16">
   <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
 </svg>
@@ -49,7 +49,7 @@ export default function Contact() {
                         <p>236 5th SE Avenue, New York NY10000, United States</p></div>
                     </div>
                 </div>
-                <div className="flex gap-2 md:w-72">
+                <div className="flex gap-4 md:w-80">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 md:size-16">
   <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
 </svg>
