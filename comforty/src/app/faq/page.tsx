45504b490d1282845536bf96971d001f6a8c9e95
m-shoaibdev <shoaibaas99@gmail.com>
2024-12-09 22:46:50 +0500

    faq color set

--- comforty/src/app/faq/page.tsx
+++ comforty/src/app/faq/page.tsx
@@ -4,62 +4,62 @@ export default function Faq() {
         <div className="lg:container lg:mx-auto px-4 lg:px-0 mt-10 md:mt-16 mb-6 md:mb-20">
             <div className="text-center">
             <h2 className="text-2xl md:text-5xl font-semibold mb-4">Questions Looks Here</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+            <p className="text-darkGray text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-10 mb-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 mb-16">
                 <div className="bg-lightGray p-6 rounded-lg">
                     <div className="flex justify-between items-center mb-4">
-                    <h3>What types of chairs do you offer?</h3>
+                    <h3 className="text-lg font-bold">What types of chairs do you offer?</h3>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
+                    <p className="text-darkGray text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
                 </div> 
                 <div className="bg-lightGray p-6 rounded-lg">
                     <div className="flex justify-between items-center mb-4">
-                    <h3>What types of chairs do you offer?</h3>
+                    <h3 className="text-lg font-bold">How can we get in touch with you?</h3>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
+                    <p className="text-darkGray text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
                 </div> 
                 <div className="bg-lightGray p-6 rounded-lg">
                     <div className="flex justify-between items-center mb-4">
-                    <h3>What types of chairs do you offer?</h3>
+                    <h3 className="text-lg font-bold">Do your chairs come with a warranty?</h3>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
+                    <p className="text-darkGray text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
                 </div> 
                 <div className="bg-lightGray p-6 rounded-lg">
                     <div className="flex justify-between items-center mb-4">
-                    <h3>What types of chairs do you offer?</h3>
+                    <h3 className="text-lg font-bold">What will be delivered? And When?</h3>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
+                    <p className="text-darkGray text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
                 </div> 
                 <div className="bg-lightGray p-6 rounded-lg">
                     <div className="flex justify-between items-center mb-4">
-                    <h3>What types of chairs do you offer?</h3>
+                    <h3 className="text-lg font-bold">Can I try a chair before purchasing?</h3>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
+                    <p className="text-darkGray text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
                 </div> 
                 <div className="bg-lightGray p-6 rounded-lg">
                     <div className="flex justify-between items-center mb-4">
-                    <h3>What types of chairs do you offer?</h3>
+                    <h3 className="text-lg font-bold">How do I clean and maintain my Comforty chair?</h3>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
+                    <p className="text-darkGray text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis modi ullam amet debitis libero veritatis enim repellat optio natus eum delectus deserunt, odit expedita eos molestiae ipsa totam quidem?</p>
                 </div> 
             </div>
         </div>
