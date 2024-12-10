174962233f0fda17b51082f268c234503cca242c
m-shoaibdev <shoaibaas99@gmail.com>
2024-12-10 13:02:12 +0500

    responsive btn bugs fixed

--- comforty/src/components/button.tsx
+++ comforty/src/components/button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export default function Button( props: ButtonProps) {
     const {title, href} = props
     return (
-        <Link href={href} className="bg-primary text-white text-sm md:text-base py-2.5 px-4 md:py-3.5 md:px-6 inline-flex gap-3.5 items-center rounded-lg">
+        <Link href={href} className="bg-primary text-white text-sm md:text-base py-2.5 px-4 md:py-3.5 md:px-6 inline-flex gap-2 md:gap-3.5 items-center rounded-lg">
                         {title}
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
--- comforty/src/components/single-featured-products.tsx
+++ comforty/src/components/single-featured-products.tsx
@@ -8,9 +8,9 @@ import Button from "./button";
 export default function SingleFeaturedProducts() {
     return (
         <div className="lg:container lg:mx-auto px-4 lg:px-0 mt-3 mb-6">
-            <div className="flex justify-between items-center mb-10">
-            <h2 className="text-2xl sm:text-3xl md:text-[32px] font-semibold ">Featured Products</h2>
-            <Button title="View All" href="all-products" />
+            <div className="flex justify-between mb-10">
+            <h2 className="text-2xl sm:text-3xl md:text-[32px] font-semibold mt-2">Featured Products</h2>
+            <Button title="View All" href="/all-products" />
             </div>
             {/* 4colums grid */}
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6 mt-6 mb-10">
